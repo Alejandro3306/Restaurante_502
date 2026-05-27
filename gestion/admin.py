04b9ca7deb648a3f35f62863e05b712e91a2e7c1
Alejandro3306 <juanalejandrorodriguez@ucundinamarca.edu.co>
2026-05-27 09:59:28 -0500

    ROLES

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Cliente, Empleado, Mesa, Plato,Orden, DetalleOrden,Factura
+from .models import Cliente, Empleado, Mesa, Plato, Orden, DetalleOrden, Factura
+
 admin.site.register(Cliente)
-admin.site.register(Empleado)   
+admin.site.register(Empleado)
 admin.site.register(Mesa)
 admin.site.register(Plato)
 admin.site.register(Orden)
